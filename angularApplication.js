461dcf58d44c7f975f3a480764407588bc7d8e43
Edmund Bayerle <e.bayerle@edba.de>
2013-02-17 19:27:23 +0100

    first working circulation page, persistence is still missing

--- angularApplication.js
+++ angularApplication.js
@@ -1,17 +1,18 @@
 'use strict';
 
-define( [ './controllers/book_loans_controller' ], function application( bookLoansController ) {
+define( [ './controllers/book_loans_controller', './services/ixoid_database' ], function application( bookLoansController, ixoidDatabase ) {
 
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    return {
       importInto: function( angularHostModule ) {
-         [bookLoansController].forEach( function( module ) {
+         console.log( ixoidDatabase );
+         [ bookLoansController, ixoidDatabase ].forEach( function( module ) {
             module.registerExports( angularHostModule );
          } );
 
       }
-   }
+   };
 
 } );
 
--- controllers/book_loans_controller.js
+++ controllers/book_loans_controller.js
@@ -1,6 +1,8 @@
 define( function() {
    'use strict';
 
+   // TODO we need access to database of books, patrons, and circulations
+   
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     function isDigit( c )
@@ -13,12 +15,23 @@ define( function() {
 
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    var controller = [ '$scope', function BookLoansController( $scope ) {
-        $scope.books = [];
-        $scope.selectedPerson = null;
+    var controller = [ '$scope', "ixoidDatabase", function BookLoansController( $scope, db ) {
+        $scope.searchedBooks = null;
+        $scope.issuedBooks = null;
+        $scope.patron = null;
         $scope.generalInputText = "";
         console.log( "BookLoansController initialized." );
 
+        var getIssuedBooks = function() {
+           db.getIssuedBooksByPatron( $scope.patron )
+           .then( function( issuedBooks ) {
+              console.log( "issuedBooks are:" );
+              console.log( issuedBooks );
+              $scope.issuedBooks = issuedBooks;
+              return issuedBooks;
+           } );
+        };
+        
         // TODO: put focus on generalInputText ?
         $scope.parseGeneralInput = function () {
             console.log( "Input is: '" + $scope.generalInputText + "'" );
@@ -26,14 +39,44 @@ define( function() {
             if( isDigit( text ) ) {
                if( text.length < 6 || text[0] == "0" ) {
                    console.log( "Person ID detected: " + text );
-                   $scope.books = [];
-               }
-               else {
+                   db.getPatronByBarcode( text )
+                   .then( function( patron ) {
+                      $scope.patron = patron;
+                      getIssuedBooks();
+                       return patron;
+                   });
+               } else {
                    console.log( "Book ID detected: " + text );
-                   $scope.books.push( text );
-               }
-            }
-            else {
+                   db.getBookByBarcode( text )
+                   .then( function( book ) {
+                     if( book ) {
+                        console.log( "found book" );
+                        if( book.issuedBy == null ) {
+                           book.issuedBy = $scope.patron;
+                           book.issuedStatus = "ISSUED";
+                        }
+                        else if( book.issuedBy == $scope.patron ) {
+                           if( book.issuedStatus == "RETURNED" ) {
+                              book.issuedStatus = "ISSUED";
+                           } else {
+                              book.issuedStatus = "RETURNED";
+                           }
+                        } else {
+                           if( book.issuedStatus == "RETURNED" ) {
+                              book.issuedStatus = "ISSUED";
+                              book.issuedBy = $scope.patron;
+                           }
+                           else {
+                              // change the patron and set to RETURNED
+                              book.issuedStatus = "RETURNED";
+                              $scope.patron = book.issuedBy;
+                           }
+                        }
+                        getIssuedBooks();
+                     }
+                   });
+               };
+            } else {
                 if( text.charAt( 0 ) == "?" ) {
                     text = text.substring( 1 ).trim();
                 }
@@ -41,7 +84,7 @@ define( function() {
             }
 
             $scope.generalInputText = "";
-        }
+        };
    } ];
 
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -51,5 +94,5 @@ define( function() {
          console.log( "BookLoansController registered." );
          angularModule.controller( 'BookLoansController', controller );
       }
-   }
+   };
 } );
